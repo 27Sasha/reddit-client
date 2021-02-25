e552a65626603ef8d38d5349ebb2930c0f0fec25
Harrison Howard <harrison.howard00707@gmail.com>
2021-02-25 11:29:20 +1000

    Display Subreddits at the top of the page when media query returns true

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,19 @@ import './App.css';
 import Header from './page/Header/Header';
 import Home from './page/Home/Home';
 import Subreddits from './page/Subreddits/Subreddits';
+import { useMediaQuery } from "react-responsive";
 
 export default function App() {
+    const isMobile = useMediaQuery({ query: "(max-width: 600px)" });
+
     return (
         <>
             <Header />
-            <main>
-                <Home />
-            </main>
-            <aside>
-                <Subreddits />
-            </aside>
+            {
+                !isMobile
+                    ? (<><main><Home /></main><aside><Subreddits /></aside></>)
+                    : (<><aside><Subreddits /></aside><main><Home /></main></>)
+            }
         </>
     );
 }
