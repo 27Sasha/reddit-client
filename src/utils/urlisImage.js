b8644e6de90e4e59aeee39b8df13083946175409
27Sasha <134843865+27Sasha@users.noreply.github.com>
2025-01-15 15:52:06 +1100

    Delete src/utils directory

--- src/utils/urlisImage.js
+++
@@ -1,8 +0,0 @@
-/**
- * Determines if the url has an extension
- * Used to check images and gifs
- * @param {string} url 
- */
-export default function urlisImage(url) {
-    return (url.match(/\.(jpeg|jpg|gif|png)$/) != null);
-}
